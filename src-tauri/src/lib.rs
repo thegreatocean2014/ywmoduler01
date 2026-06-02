8ddd7a39df42369e674a92e0063982acfb9b8156
thegreatocean2014 <553878259@qq.com>
2026-06-02 10:20:30 -0500

    添加数据库模块

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,6 +18,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_fs::init())
+        // .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(
             tauri_plugin_log::Builder::default()
                 .level(log::LevelFilter::Info)
